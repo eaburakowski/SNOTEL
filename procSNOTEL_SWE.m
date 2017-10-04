3a73f71b58c2293c348e73880bcacf038a52a198
Elizabeth Burakowski <elizabeth.burakowski@gmail.com>
2017-10-04 13:40:55 -0400

    Add files via upload
    Calculates maximum snow water equivalent (SWEmax) and sum of SWE (SWEsum) for individual US SNOTEL stations, calculates a statewide average SWEmax and SWEsum, and ranks snow seasons lowest to highest based on SWEmax and SWEsum.  
    
    Requires a state-level SNOTEL stations .mat file generated with procSNOTEL.m, and user-defined data_thresh for missing values within a given season, and yr_thresh for missing years within a time series (currently 2000-2016).

--- procSNOTEL_SWE.m
+++ procSNOTEL_SWE.m
@@ -45,15 +45,20 @@ states = cellstr(states);
 % go to SNOTEL .mat directory
 cd /Users/zubeneschamali/Documents/Data/SNOTEL/mat
 
-% define start year, end year, US states, and climate variable
+% define start year, end year, other constants
 yrsrt = 2000;
 yrend = 2016;
+years = (yrsrt:yrend)';
+data_thresh = 0.25; % winter missing data threshold
+yr_thresh = 2;      % time series missing years threshold 
 
 % Loop over states and SNOTEL sites
 % Calculate missing data
 fils = dir('*.mat');
+
 for istate = 1:length(states)
-    load(fils(istate).name)
+    load(fils(istate).name);
+    fils(istate).name
     
     % list station IDs
     stations = who('snotel_*');
@@ -61,13 +66,88 @@ for istate = 1:length(states)
     
     % Loop over stations within state
     for istation = 1:length(stations)
+        
+        % pull out station daily SWE data and convert to meters
         data = eval(stations{istation});
         SWE = data.SWEmm;
-        date = data.Date; % NEED TO CONVERT FROM STRING TO YYYY MM DD
+        SWE(find(SWE==-9999))=NaN;
+        SWE = SWE/1000;
         
+        % pull out dates and split into year and month
+        date = str2double(split(data.Date,'-'));
+        year = date(:,1);
+        month = date(:,2);
         
+        % Loop over years, pull out one winter of SWE data
+        for iyr = 1:length(yrsrt:yrend)-1
+            onewinter = SWE(find(year==years(iyr)&month>=11|year==years(iyr+1)&month<=4));
+            if (length(find(isnan(onewinter)))/length(onewinter))<data_thresh
+                SWEsum(iyr,istation)=nansum(onewinter);
+                SWEmax(iyr,istation)=max(onewinter);
+            else
+                SWEsum(iyr,istation)=NaN;
+                SWEmax(iyr,istation)=NaN;
+            end
+        end
+        
+        clear data SWE date year month SNWD
     end
     
+    % Eliminate stations for which missing years (NaN) is > yr_thresh
+    SWEsum_remove = [];
+    for istation = 1:length(stations)
+        if length(find(isnan(SWEsum(:,istation))))>yr_thresh
+            SWEsum_remove = [SWEsum_remove;istation];
+        else
+            SWEsum_remove = SWEsum_remove;
+        end
+    end
+    SWEsum(:,SWEsum_remove)=[];
+ 
+    % Eliminate stations for which missing years (NaN) is > yr_thresh
+    SWEmax_remove = [];
+    for istation = 1:length(stations)
+        if length(find(isnan(SWEmax(:,istation))))>yr_thresh
+            SWEmax_remove = [SWEmax_remove;istation];
+        else
+            SWEmax_remove = SWEmax_remove;
+        end
+    end
+    SWEmax(:,SWEmax_remove)=[];
+    
+    % Calculate statewide average snowdays
+    State_SWEsum(:,istate) = nanmean(SWEsum,2);
+    State_SWEmax(:,istate) = nanmean(SWEmax,2);
+    
+    %Plot statewide SWEsum and SWEmax per winter, 2000-2016
+        figure(istate); clf
+        set(gcf,'Color','white')
+        set(gca,'FontSize',20)
+    
+        % y-axis
+        yrs2 = 2001:2016;
+    
+        % plot time series
+        p = plotyy(yrs2,State_SWEsum(:,istate),yrs2,State_SWEmax(:,istate))
+    
+        % plot options
+        xlabel('Year Dec-Mar')
+        ylabel(p(1),'SWE total (m)')
+        ylabel(p(2),'SWE max (m)')
+        title(states(istate,:));
+        legend('SWEsum','SWEmax','Location','NorthEast')
+        text(2001,max(SWEmax(:,istate)),sprintf('n = %d ',length(SWEsum(1,:))))
+    
+    % Rank statewide snow days, lowest to highest
+    [rank, irank] = sort(SWEsum(:,istate));
+    State_SWEsum_Rankings(:,istate) = yrs2(irank);
+    
+    [rank, irank] = sort(SWEmax(:,istate));
+    State_SWEmax_Rankings(:,istate) = yrs2(irank);
+    
+    % Clean up
+    clear stations SWEsum_remove SWEmax_remove
+    clear snotel_*
     
 end
 
